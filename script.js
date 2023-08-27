bd0e7b71fc41c0d40c46502b54aa4c37427ef752
Pruthviraj <91754763+Pruthvi2121@users.noreply.github.com>
2023-08-28 00:41:13 +0530

    initial commmit

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function () {
+  
+  
+//   });
+
+var swiper = new Swiper(".image_slider", {
+    direction: "vertical",
+    centeredSlides: true,
+    loop:true,
+    speed:10000,
+    // freeMode: true,
+    allowTouchMove: false,
+    // cssMode: true,
+    autoplay: {
+      delay:100,
+      
+    },
+    
+    pauseOnInteraction: false
+  });
